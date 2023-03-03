d9252c80fc0d5505d36fccf8b29fac0d3666ed99
yuutoyasue <uk27fbk@yahoo.co.jp>
2023-03-03 23:04:05 +0900

    新規

---
+++ src/main/java/com/techacademy/TutorialDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialDevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialDevelopApplication.class, args);
+	}
+
+}
